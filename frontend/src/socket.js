127a59ef053339e99d240379749d8e40571a1fdf
Anas Saifi <anaskhan7668014201@gmail.com>
2026-01-03 04:09:57 +0530

    default export

--- frontend/src/socket.js
+++ frontend/src/socket.js
@@ -2,7 +2,9 @@ import { io } from "socket.io-client";
 
 const API_BASE = import.meta.env.VITE_API_URL;
 
-export const socket = io(API_BASE, {
+const socket = io(API_BASE, {
   autoConnect: false,
   transports: ["websocket", "polling"],
 });
+
+export default socket;
